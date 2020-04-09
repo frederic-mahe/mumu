494b7d856b3a612b59db0e8241bc89d2efdc8312
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2020-04-09 15:26:51 +0200

    add license

--- src/mumu.cpp
+++ src/mumu.cpp
@@ -1,4 +1,28 @@
+// MUMU
+
+// Copyright (C) 2020 Frederic Mahe
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Contact: Frederic Mahe <frederic.mahe@cirad.fr>,
+// UMR BGPI, CIRAD - TA A-54/K
+// Campus International de Baillarguet
+// 34398 MONTPELLIER CEDEX 5
+// France
+
 #include <iostream>
+
 
 auto main() -> int {
   std::cout << "Hello World!" << '\n';
