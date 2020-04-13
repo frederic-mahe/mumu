69917acf1e9b4742324cc7ee9198d94326782f54
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2020-04-13 16:45:53 +0200

    command line interface (all options)

--- src/cli.cpp
+++ src/cli.cpp
@@ -22,40 +22,104 @@
 // France
 
 #include <iostream>
+#include <array>
+#include <string>
+#include <cstdlib>  // atoi, atof
 #include <getopt.h>
+
+constexpr auto threads_default {1U};
+constexpr auto minimum_match_default {84U};
+constexpr auto minimum_relative_cooccurence_default {0.95F};
+constexpr auto minimum_ratio_default {1.0F};
+
+// 12 options
+const struct option long_options[] =
+  {// standard options
+   {"help",    no_argument,       nullptr, 'h'},
+   {"threads", required_argument, nullptr, 't'},
+   {"version", no_argument,       nullptr, 'v'},
+
+   // input
+   {"otu_table",  required_argument, nullptr, 'o'},
+   {"match_list", required_argument, nullptr, 'm'},
+
+   // parameters
+   {"minimum_match",      required_argument, nullptr, 'a'},
+   {"minimum_ratio_type", required_argument, nullptr, 'b'},
+   {"minimum_ratio",      required_argument, nullptr, 'c'},
+   {"minimum_relative_cooccurence", required_argument, nullptr, 'd'},
+
+   // output
+   {"new_otu_table", required_argument, nullptr, 'n'},
+   {"log",           required_argument, nullptr, 'l'},
+
+   {nullptr, 0, nullptr, 0}
+  };
+
+
+struct Parameters {
+  // mandatory arguments
+  bool is_otu_table {false};
+  bool is_match_list {false};
+  bool is_new_otu_table {false};
+  bool is_log {false};
+  std::string otu_table;
+  std::string match_list;
+  std::string new_otu_table;
+  std::string log;
+
+  // default values
+  unsigned int threads {threads_default};
+  unsigned int minimum_match {minimum_match_default};
+  std::string minimum_ratio_type {"min"};
+  double minimum_ratio {minimum_ratio_default};
+  double minimum_relative_cooccurence {minimum_relative_cooccurence_default};
+};
+
+
+void help () {
+  std::cout << "Usage: mumu\n"
+            << " -h, --help                           display this help and exit\n"
+            << " -v, --version                        display version information and exit\n"
+            << " -t, --threads INTEGER                number of threads to use (1)\n"
+            << "\n"
+            << "Input options (mandatory):\n"
+            << " --otu_table FILE                     tab-separated, samples in columns\n"
+            << " --match_list FILE                    tab-separated, OTU pairwise similarity scores\n"
+            << "\n"
+            << "Output options (mandatory):\n"
+            << " --new_otu_table FILE                 write an updated OTU table\n"
+            << " --log FILE                           record operations\n"
+            << "\n"
+            << "Computation parameters:\n"
+            << " --minimum_match INTEGER              minimum similarity threshold (84)\n"
+            << " --minimum_ratio FLOAT                minimum abundance ratio (1.0)\n"
+            << " --minimum_ratio_type STRING          \"min\" or \"avg\" abundance ratio (\"min\")\n"
+            << " --minimum_relative_cooccurence FLOAT relative father-son spread (0.95)\n"
+            << "\n";
+  std::exit(EXIT_SUCCESS);
+}
+
+
+void version() {
+  std::cout << "mumu 0.0.1\n"
+            << "Copyright (C) 2020 Frederic Mahe\n"
+            << "https://github.com/frederic-mahe/mumu\n"
+            << "\n";
+  std::exit(EXIT_SUCCESS);
+}
+
 
 void parse_args (int argc, char ** argv)
 {
 
   auto c {0};
-
-  const struct option long_options[] = {// standard options
-                                        {"help",    no_argument, nullptr, 'h'},
-                                        {"threads",  required_argument, nullptr, 't'},
-                                        {"version", no_argument, nullptr, 'v'},
-
-                                        // input
-                                        {"otu_table", required_argument, nullptr, 0},
-                                        {"match_list", required_argument, nullptr, 0},
-
-                                        // parameters
-                                        {"minimum_match", required_argument, nullptr, 0},
-                                        {"minimum_ratio_type", required_argument, nullptr, 0},
-                                        {"minimum_ratio", required_argument, nullptr, 0},
-                                        {"minimum_relative_cooccurence", required_argument, nullptr, 0},
-                                        // output
-                                        {"new_otu_table", required_argument, nullptr, 0},
-                                        {"log", required_argument, nullptr, 0},
-
-                                        {nullptr, 0, nullptr, 0}
-  };
-
-
+  Parameters parameters;
 
   while (true) {
     auto option_index {0};
 
-    c = getopt_long(argc, argv, "ht:v", long_options, &option_index);
+    c = getopt_long(argc, argv, "a:b:c:d:hl:m:n:o:t:v", long_options, &option_index);
     if (c == -1) {
       break;
     }
@@ -69,16 +133,52 @@ void parse_args (int argc, char ** argv)
       std::cout << "\n";
       break;
 
-    case 'h':
-      std::cout << "option h\n";
+    case 'a':  // minimum match (default is 84)
+      parameters.minimum_match = atoi(optarg);
       break;
 
-    case 't':
-      std::cout << "option t with value " << optarg << "\n";
+    case 'b':  // minimum ratio type (default is "min")
+      parameters.minimum_ratio_type = optarg;
       break;
 
-    case 'v':
-      std::cout << "option v\n";
+    case 'c':  // minimum ratio (default is 1.0)
+      parameters.minimum_ratio = atof(optarg);
+      break;
+
+    case 'd':  // minimum relative cooccurence (default is 0.95)
+      parameters.minimum_ratio = atof(optarg);
+      break;
+
+    case 'h':  // help message
+      help();
+      break;
+
+    case 'l':  // log file (output)
+      parameters.log = optarg;
+      parameters.is_log = true;
+      break;
+
+    case 'm':  // match list file (input)
+      parameters.match_list = optarg;
+      parameters.is_match_list = true;
+      break;
+
+    case 'n':  // new OTU table file (output)
+      parameters.new_otu_table = optarg;
+      parameters.is_new_otu_table = true;
+      break;
+
+    case 'o':  // OTU table file (input)
+      parameters.otu_table = optarg;
+      parameters.is_otu_table = true;
+      break;
+
+    case 't':  // threads (default is 1)
+      parameters.threads = atoi(optarg);
+      break;
+
+    case 'v':  // version number
+      version();
       break;
 
     case '?':
