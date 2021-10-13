807383344fd844bc37e6d3ce48a35d17e2551bbb
Frédéric Mahé <frederic.mahe@cirad.fr>
2021-10-13 12:59:07 +0200

    comment for future refactoring

--- src/mumu.h
+++ src/mumu.h
@@ -34,7 +34,7 @@ static_assert(UINT_MAX >= UINT32_MAX, "unsigned integers are too small");
 
 constexpr auto sepchar {'\t'};
 constexpr auto n_version {"0.0.1"};
-constexpr auto copyright_years {"2020-2021"};
+constexpr auto copyright_years {"2020-2021"};  // convert to string_view
 constexpr auto threads_default {1U};
 constexpr auto minimum_match_default {84.0};
 constexpr auto minimum_relative_cooccurence_default {0.95};
