2a3d242bc2ede34a488b87ba1ae5261cda785d47
Frédéric Mahé <frederic.mahe@cirad.fr>
2022-02-25 17:47:59 +0100

    version number: first stable release

--- src/mumu.h
+++ src/mumu.h
@@ -33,7 +33,7 @@ static_assert(UINT_MAX > UINT16_MAX, "unsigned integers are too small");
 static_assert(UINT_MAX >= UINT32_MAX, "unsigned integers are too small");
 
 constexpr auto sepchar {'\t'};
-constexpr std::string_view n_version {"0.0.1"};
+constexpr std::string_view n_version {"1.0.0"};
 constexpr std::string_view copyright_years {"2020-2022"};
 constexpr auto threads_default {1U};
 constexpr auto minimum_match_default {84.0};
