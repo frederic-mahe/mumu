9245e8e5b0f8151f8187e35157b2f081b72ce630
Frédéric Mahé <frederic.mahe@cirad.fr>
2022-06-29 12:26:03 +0200

    fix coverage computation

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -6,7 +6,7 @@ gcov ./src/*.cpp 2> /dev/null | \
     awk 'BEGIN {FS = "[: %]"}
          /^Lines executed/ {
             total_lines += $6
-            missed_lines = $6 * $3 / 100.0
+            missed_lines += $6 - ($6 * $3 / 100.0)
          }
          END {
              printf "%.0f\n", 100.0 * (total_lines - missed_lines) / total_lines
