9f3ea3f2efa92d5c17fb943e604bb790d0b7f503
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-03-23 18:16:46 +0100

    avoid printing giant values (log file)

--- src/search_parent.cpp
+++ src/search_parent.cpp
@@ -138,8 +138,12 @@ auto test_parents (std::unordered_map<std::string, struct OTU> &OTUs,
     if (stats.father_overlap_spread > 0) {  // avoid dividing by zero
       stats.avg_non_null_ratio = stats.sum_ratio / stats.father_overlap_spread;
     }
+    // avoid printing giant values
+    if (largest_double - stats.smallest_ratio <= tolerance) {
+      stats.smallest_ratio = 0.0;
+    }
     if (largest_double - stats.smallest_non_null_ratio <= tolerance) {
-      stats.smallest_non_null_ratio = 0.0;  // avoid printing a giant value
+        stats.smallest_non_null_ratio = 0.0;
     }
 
     // not a parent if...
