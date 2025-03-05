56d16b6580089cfd910b35d580aad2ab33d41ff8
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-03-05 11:30:55 +0100

    spacing

--- src/validate_args.cpp
+++ src/validate_args.cpp
@@ -29,83 +29,83 @@
 
 
 namespace {
-auto check_mandatory_arguments(Parameters const &parameters) -> void {
-  if (not parameters.is_otu_table) {
-    fatal("missing mandatory argument --otu_table filename");
-  }
-  if (not parameters.is_match_list) {
-    fatal("missing mandatory argument --match_list filename");
-  }
-  if (not parameters.is_new_otu_table) {
-    fatal("missing mandatory argument --new_otu_table filename");
-  }
-  if (not parameters.is_log) {
-    fatal("missing mandatory argument --log filename");
-  }
-}
-
-
-auto input_files_are_reachable(Parameters const &parameters) -> void {
-  for (const auto& file_name : {parameters.otu_table, parameters.match_list} ) {
-    const std::ifstream input_file {file_name};
-    if (not input_file) {
-      fatal("can't open input file " + file_name);
+  auto check_mandatory_arguments(Parameters const &parameters) -> void {
+    if (not parameters.is_otu_table) {
+      fatal("missing mandatory argument --otu_table filename");
+    }
+    if (not parameters.is_match_list) {
+      fatal("missing mandatory argument --match_list filename");
+    }
+    if (not parameters.is_new_otu_table) {
+      fatal("missing mandatory argument --new_otu_table filename");
+    }
+    if (not parameters.is_log) {
+      fatal("missing mandatory argument --log filename");
     }
   }
-}
 
 
-auto output_files_are_writable(Parameters const &parameters) -> void {
-  for (const auto& file_name : {parameters.new_otu_table, parameters.log} ) {
-    const std::ofstream output_file {file_name};
-    if (not output_file) {
-      fatal("can't open input file " + file_name);
+  auto input_files_are_reachable(Parameters const &parameters) -> void {
+    for (const auto& file_name : {parameters.otu_table, parameters.match_list} ) {
+      const std::ifstream input_file {file_name};
+      if (not input_file) {
+        fatal("can't open input file " + file_name);
+      }
     }
   }
-}
 
 
-auto check_numerical_parameters(Parameters const &parameters) -> void {
-  // minimum match (50 <= x <= 100)
-  constexpr static auto lowest_similarity {50.0};
-  constexpr static auto highest_similarity {100.0};
-  if (parameters.minimum_match < lowest_similarity
-      or parameters.minimum_match > highest_similarity) {
-    fatal("--minimum_match value must be between " +
-          std::to_string(lowest_similarity) +
-          " and " +
-          std::to_string(highest_similarity));
+  auto output_files_are_writable(Parameters const &parameters) -> void {
+    for (const auto& file_name : {parameters.new_otu_table, parameters.log} ) {
+      const std::ofstream output_file {file_name};
+      if (not output_file) {
+        fatal("can't open input file " + file_name);
+      }
+    }
   }
 
-  // minimum ratio (x > 0)
-  if (parameters.minimum_ratio <= 0) {
-    fatal("--minimum_ratio value must be greater than zero");
-  }
 
-  // minimum relative cooccurence (0 < x <= 1)
-  if (parameters.minimum_relative_cooccurence <= 0.0 or
-      parameters.minimum_relative_cooccurence > 1.0) {
-        fatal("--minimum_relative_cooccurence value must be between zero and one");
-  }
+  auto check_numerical_parameters(Parameters const &parameters) -> void {
+    // minimum match (50 <= x <= 100)
+    constexpr static auto lowest_similarity {50.0};
+    constexpr static auto highest_similarity {100.0};
+    if (parameters.minimum_match < lowest_similarity
+        or parameters.minimum_match > highest_similarity) {
+      fatal("--minimum_match value must be between " +
+            std::to_string(lowest_similarity) +
+            " and " +
+            std::to_string(highest_similarity));
+    }
 
-  // threads (1 <= x <= 255)
-  constexpr static auto max_threads {255};
-  if (parameters.threads != 1) {
-    std::cout << "warning: mumu is not yet multithreaded.\n";
-  }
-  if (parameters.threads < 1 or parameters.threads > max_threads) {
-    fatal("--threads value must be between 1 and " + std::to_string(max_threads));
-  }
+    // minimum ratio (x > 0)
+    if (parameters.minimum_ratio <= 0) {
+      fatal("--minimum_ratio value must be greater than zero");
+    }
 
-  // minimum ratio type ("min" or "avg")  // replace != with not_eq?
-  if (parameters.minimum_ratio_type != use_minimum_value and
-      parameters.minimum_ratio_type != use_average_value) {
-    fatal("--minimum ratio type can only be " +
-          std::string{use_minimum_value} +
-          "\" or \"" +
-          std::string{use_average_value});
+    // minimum relative cooccurence (0 < x <= 1)
+    if (parameters.minimum_relative_cooccurence <= 0.0 or
+        parameters.minimum_relative_cooccurence > 1.0) {
+      fatal("--minimum_relative_cooccurence value must be between zero and one");
+    }
+
+    // threads (1 <= x <= 255)
+    constexpr static auto max_threads {255};
+    if (parameters.threads != 1) {
+      std::cout << "warning: mumu is not yet multithreaded.\n";
+    }
+    if (parameters.threads < 1 or parameters.threads > max_threads) {
+      fatal("--threads value must be between 1 and " + std::to_string(max_threads));
+    }
+
+    // minimum ratio type ("min" or "avg")  // replace != with not_eq?
+    if (parameters.minimum_ratio_type != use_minimum_value and
+        parameters.minimum_ratio_type != use_average_value) {
+      fatal("--minimum ratio type can only be " +
+            std::string{use_minimum_value} +
+            "\" or \"" +
+            std::string{use_average_value});
+    }
   }
-}
 } // namespace
 
 
