e29a74cdad885b88e2d2e758893daba154844ee1
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-03-05 11:38:00 +0100

    spacing

--- src/load_OTUs.cpp
+++ src/load_OTUs.cpp
@@ -35,59 +35,59 @@
 
 
 namespace {
-[[nodiscard]]
-auto count_samples (const std::string &line) -> unsigned int {
-  // number of column separators is equal to the number of samples
-  return static_cast<unsigned int>(std::ranges::count(line, sepchar));
-}
-
-
-auto check_number_of_samples(unsigned int const number_of_samples) -> void {
-  if (number_of_samples == 0) {
-    std::cout << "\nwarning: OTU table should have at least one sample\n";
-  }
-}
-
-
-auto output_first_line (const std::string &line,
-                        struct Parameters const &parameters) -> void {
-  // write header line to new OTU table
-  std::ofstream new_otu_table {parameters.new_otu_table};
-  new_otu_table << line << '\n';
-}
-
-
-auto parse_each_otu (std::unordered_map<std::string, struct OTU> &OTUs,
-                     std::string &line,
-                     const unsigned int n_samples) -> void {
-  // get OTU id
-  const auto first_sep {line.find_first_of(sepchar)};
-  const std::string OTU_id {line.substr(0, first_sep)};
-
-  // check for duplicates
-  if (OTUs.contains(OTU_id)) {
-    fatal("duplicated OTU name: " + OTU_id);
+  [[nodiscard]]
+  auto count_samples (const std::string &line) -> unsigned int {
+    // number of column separators is equal to the number of samples
+    return static_cast<unsigned int>(std::ranges::count(line, sepchar));
   }
 
-  // get abundance values (rest of the line, we know there are n samples)
-  OTU otu;
-  otu.samples.reserve(n_samples);
-  std::stringstream abundances {line.substr(first_sep + 1)};
-  for (const auto abundance : std::ranges::istream_view<unsigned long int>(abundances)) {
-        otu.samples.push_back(abundance);
+
+  auto check_number_of_samples(unsigned int const number_of_samples) -> void {
+    if (number_of_samples == 0) {
+      std::cout << "\nwarning: OTU table should have at least one sample\n";
+    }
   }
 
-  // sanity check
-  if (otu.samples.size() != n_samples) {
-    fatal("variable number of columns in OTU table");
+
+  auto output_first_line (const std::string &line,
+                          struct Parameters const &parameters) -> void {
+    // write header line to new OTU table
+    std::ofstream new_otu_table {parameters.new_otu_table};
+    new_otu_table << line << '\n';
   }
 
-  // add more results to the map
-  auto has_reads = [](const auto n_reads) { return n_reads > 0; };
-  otu.spread = static_cast<unsigned int>(std::ranges::count_if(otu.samples, has_reads));
-  otu.sum_reads = std::accumulate(otu.samples.begin(), otu.samples.end(), 0UL);
-  OTUs[OTU_id] = std::move(otu);
-}
+
+  auto parse_each_otu (std::unordered_map<std::string, struct OTU> &OTUs,
+                       std::string &line,
+                       const unsigned int n_samples) -> void {
+    // get OTU id
+    const auto first_sep {line.find_first_of(sepchar)};
+    const std::string OTU_id {line.substr(0, first_sep)};
+
+    // check for duplicates
+    if (OTUs.contains(OTU_id)) {
+      fatal("duplicated OTU name: " + OTU_id);
+    }
+
+    // get abundance values (rest of the line, we know there are n samples)
+    OTU otu;
+    otu.samples.reserve(n_samples);
+    std::stringstream abundances {line.substr(first_sep + 1)};
+    for (const auto abundance : std::ranges::istream_view<unsigned long int>(abundances)) {
+      otu.samples.push_back(abundance);
+    }
+
+    // sanity check
+    if (otu.samples.size() != n_samples) {
+      fatal("variable number of columns in OTU table");
+    }
+
+    // add more results to the map
+    auto has_reads = [](const auto n_reads) { return n_reads > 0; };
+    otu.spread = static_cast<unsigned int>(std::ranges::count_if(otu.samples, has_reads));
+    otu.sum_reads = std::accumulate(otu.samples.begin(), otu.samples.end(), 0UL);
+    OTUs[OTU_id] = std::move(otu);
+  }
 } // namespace
 
 
