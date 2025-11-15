45e75bd6fb27cf751acf4cb034fd28cf90ad3d57
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-11-15 10:13:01 +0100

    lulu does chain merging, no need to make it conditional

--- src/search_parent.cpp
+++ src/search_parent.cpp
@@ -171,12 +171,6 @@ namespace {
                    .son_spread = otu.spread,
                    .father_spread = father.spread};  // refactoring: son's stats should be initialized outside of the loop, or separated into another struct
 
-      // reject: replicate lulu's behavior (no chained merges)
-      if (parameters.is_legacy and father.is_mergeable) {
-        // skip parent OTUs that can be merged with another ancestor
-        continue;
-      }
-
       // compute father/son ratios for all samples
       per_sample_ratios(OTUs, stats);
 
