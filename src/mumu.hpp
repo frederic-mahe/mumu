34cad654cdcabd83eb7a14d1b196dd3b9507b893
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-01-31 17:10:46 +0100

    update copyright year

--- src/mumu.hpp
+++ src/mumu.hpp
@@ -1,6 +1,6 @@
 // MUMU
 
-// Copyright (C) 2020-2024 Frederic Mahe
+// Copyright (C) 2020-2026 Frederic Mahe
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -37,7 +37,7 @@ static_assert(UINT_MAX >= UINT32_MAX, "unsigned integers are too small");
 // refactoring: reduce scope of these constants
 constexpr auto sepchar {'\t'};
 constexpr std::string_view n_version {"1.1.0"};
-constexpr std::string_view copyright_years {"2020-2025"};
+constexpr std::string_view copyright_years {"2020-2026"};
 constexpr auto threads_default {1U};
 constexpr auto minimum_match_default {84.0};
 constexpr auto minimum_relative_cooccurence_default {0.95};
