0dcd8a15aaafbad424180d6203fdf8873b048b99
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-02-05 11:54:40 +0100

    update version number

--- src/mumu.hpp
+++ src/mumu.hpp
@@ -36,7 +36,7 @@ static_assert(UINT_MAX >= UINT32_MAX, "unsigned integers are too small");
 
 // refactoring: reduce scope of these constants
 constexpr auto sepchar {'\t'};
-constexpr std::string_view n_version {"1.1.1"};
+constexpr std::string_view n_version {"1.1.2"};
 constexpr std::string_view copyright_years {"2020-2026"};
 constexpr auto threads_default {1U};
 constexpr auto minimum_match_default {84.0};
